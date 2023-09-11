b2b4bf6e2b9759c3f82cfa0823d1d8a8b08f3095
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-11 20:26:48 +0300

    setting unique categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+
 import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
 
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
+
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories)
   return <main>
   <section className="menu">
   <Title text={'Daniel-Menu'}/>
