c1695e992fe626545f1ba5e1ad1c4d34f78f007e
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-12 11:25:48 +0300

    finishing project with all button

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Categories = ({categories}) => {
+const Categories = ({categories, filterItems}) => {
   return (
     <div className='btn-container'>
     {categories.map((category) => {
-        return <button type='button' className='btn' key={category}>
+        return <button type='button' className='btn' key={category} onClick={() => filterItems(category)}>
             {category}
         </button>
     })}
