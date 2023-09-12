de4b4edc5cd466d8dd71a41a4347d88ba3c5e058
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-12 11:18:43 +0300

    adding categories buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
+import Categories from "./Categories";
 
 
 const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
@@ -11,10 +12,11 @@ const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
-  console.log(categories)
+  
   return <main>
   <section className="menu">
   <Title text={'Daniel-Menu'}/>
+  <Categories categories={categories}/>
   <Menu items={menuItems}/>
   </section>
   </main>;
